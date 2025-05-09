0e0a3b8eba98c07609257a3ecd50ee06e5a42695
Lark Aster <lark@lark.gay>
2025-05-09 00:19:32 -0400

    Fix Terraform connection string building

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,11 +1,11 @@
 output "secrets" {
   value = {
     for env, config in local.environments : config.fly_app => {
-      # The `ssl=true` is necessary for NocoDB to talk to Neon.
+      # The `ssl=true` param is important.
       NC_DB = "pg://${neon_project.env[env].database_host_pooler}:5432?u=${neon_project.env[env].database_user}&p=${neon_project.env[env].database_password}&d=${neon_project.env[env].database_name}&ssl=true"
 
-      # The `family=6` is necessary for NocoDB to talk to Upstash.
-      NC_REDIS_URL = "redis://default:${upstash_redis_database.noco[env].password}@${upstash_redis_database.noco[env].endpoint}:${upstash_redis_database.noco[env].port}/?family=6"
+      # The extra 's' in the `rediss://` scheme is important.
+      NC_REDIS_URL = "rediss://default:${upstash_redis_database.noco[env].password}@${upstash_redis_database.noco[env].endpoint}:${upstash_redis_database.noco[env].port}"
     }
   }
   sensitive = true
