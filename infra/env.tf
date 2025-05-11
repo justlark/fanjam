3d883db0d847fa47679e327c3a07a958e0dfbb59
Lark Aster <lark@lark.gay>
2025-05-11 15:20:54 -0400

    Pass Neon API token into worker

--- infra/env.tf
+++ infra/env.tf
@@ -1,4 +1,6 @@
 locals {
+  stages = toset(["prod", "test"])
+
   environments = {
     for env_file in fileset("${path.module}/environments", "*/env.yaml") : dirname(env_file) => yamldecode(file("${path.module}/environments/${env_file}"))
   }
---
+++ infra/worker.tf
@@ -0,0 +1,18 @@
+resource "cloudflare_workers_secret" "neon_api_token" {
+  for_each = local.stages
+
+  account_id  = var.cloudflare_account_id
+  name        = "NEON_API_TOKEN"
+  script_name = "sparklefish-server-${each.key}"
+  secret_text = var.neon_api_token
+}
+
+# This isn't really a secret, but it's convenient to pass it here.
+resource "cloudflare_workers_secret" "neon_org_id" {
+  for_each = local.stages
+
+  account_id  = var.cloudflare_account_id
+  name        = "NEON_ORG_ID"
+  script_name = "sparklefish-server-${each.key}"
+  secret_text = var.neon_org_id
+}
