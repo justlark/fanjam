5c93acad8374b58398d934616b0255751d1de59a
Lark Aster <lark@lark.gay>
2025-05-19 21:01:58 -0400

    Add TODO comment

--- infra/neon.tf
+++ infra/neon.tf
@@ -2,6 +2,11 @@ locals {
   neon_default_branch_name = "main"
 }
 
+// TODO: Once this issue in the Neon Terraform provider is addressed, we can
+// protect the default branch so it cannot be deleted.
+//
+// https://github.com/kislerdm/terraform-provider-neon/issues/153
+
 resource "neon_project" "env" {
   for_each = local.environments
   name     = each.key
