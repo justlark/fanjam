fdefe599c122ba3d388438bc4640d3ef61ee2216
Lark Aster <lark@lark.gay>
2025-05-21 10:31:15 -0400

    Set up Tailwind

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-4xl text-center mt-4">FanJam</h1>
   </main>
 </template>
--- client/worker-configuration.d.ts
+++ client/worker-configuration.d.ts
@@ -2,5 +2,5 @@
 // After adding bindings to `wrangler.jsonc`, regenerate this interface via `npm run cf-typegen`
 
 interface Env {
-	ASSETS: Fetcher;
+  ASSETS: Fetcher;
 }
