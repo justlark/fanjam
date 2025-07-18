a9d36d18e6f6b97aa755469cea9b74352df83826
Lark Aster <lark@lark.gay>
2025-07-18 06:21:56 -0400

    Add CategoryLabel component

---
+++ client/src/components/CategoryLabel.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import Tag from "primevue/tag";
+import { bgColorForString, fgColorForString } from "@/utils/colors";
+
+const props = defineProps<{
+  category: string;
+}>();
+
+const bgColor = computed(() => bgColorForString(props.category, 100));
+const fgColor = computed(() => fgColorForString(props.category, 500));
+</script>
+
+<template>
+  <Tag :value="props.category" :class="`!${fgColor} !${bgColor}`" />
+</template>
---
+++ client/src/utils/colors.ts
@@ -0,0 +1,48 @@
+// A hash algorithm that converts a string to a number.
+const djb2Hash = (str: string) => {
+  // This magic number is specified by the djb2 algorithm; we didn't choose it,
+  // and shouldn't try to change it.
+  let hash = 5381;
+
+  for (let i = 0; i < str.length; i++) {
+    hash = (hash << 5) + hash + str.charCodeAt(i);
+  }
+
+  return hash >>> 0;
+};
+
+const allPaletteColors = [
+  "emerald",
+  "green",
+  "lime",
+  "red",
+  "orange",
+  "amber",
+  "yellow",
+  "teal",
+  "cyan",
+  "sky",
+  "blue",
+  "indigo",
+  "violet",
+  "purple",
+  "fuchsia",
+  "pink",
+  "rose",
+];
+
+const deterministicRandomColor = (str: string) => {
+  const hash = djb2Hash(str);
+  const index = hash % allPaletteColors.length;
+  return allPaletteColors[index];
+};
+
+export const fgColorForString = (str: string, value: number) => {
+  const color = deterministicRandomColor(str);
+  return `text-${color}-${value}`;
+};
+
+export const bgColorForString = (str: string, value: number) => {
+  const color = deterministicRandomColor(str);
+  return `bg-${color}-${value}`;
+};
