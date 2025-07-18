9ccdb5107661fc521541e02b9204e8e3ff6200d5
Lark Aster <lark@lark.gay>
2025-07-18 01:58:09 -0400

    Add PrimeVue

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from "primevue/button";
+</script>
 
 <template>
-  <main>
+  <main class="flex flex-col justify-center items-center gap-8">
     <h1 class="text-4xl text-center mt-4">FanJam</h1>
+    <Button label="Example" />
   </main>
 </template>
