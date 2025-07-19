35fd18c54b7fa21d201bb7cd5ec75be552e8c7cb
Lark Aster <lark@lark.gay>
2025-07-18 21:04:13 -0400

    Make API call to get events on the client

---
+++ client/src/utils/api.ts
@@ -0,0 +1,54 @@
+interface RawEvent {
+  name: string;
+  description: string;
+  start_time: string;
+  end_time: string;
+  location: string;
+  people: Array<string>;
+  category: string;
+  tags: Array<string>;
+}
+
+export interface Event {
+  name: string;
+  description: string;
+  startTime: Date;
+  endTime: Date;
+  location: string;
+  people: Array<string>;
+  category: string;
+  tags: Array<string>;
+}
+
+export type ApiResponse<T> =
+  | {
+    ok: true;
+    value: T;
+  }
+  | {
+    ok: false;
+    status: number;
+  };
+
+export const getEvents = async (envId: string): Promise<ApiResponse<Array<Event>>> => {
+  const response = await fetch(`https://${import.meta.env.VITE_API_HOST}/events/${envId}`);
+
+  if (!response.ok) {
+    return { ok: false, status: response.status };
+  }
+
+  const rawEvents: Array<RawEvent> = await response.json();
+
+  const events: Array<Event> = rawEvents.map((event) => ({
+    name: event.name,
+    description: event.description,
+    startTime: new Date(event.start_time),
+    endTime: new Date(event.end_time),
+    location: event.location,
+    people: event.people,
+    category: event.category,
+    tags: event.tags,
+  }));
+
+  return { ok: true, value: events };
+};
