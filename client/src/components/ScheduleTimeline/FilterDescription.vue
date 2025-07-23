7bc4f0cdc7b4a446d94fb4b600e06c837fb37309
Lark Aster <lark@lark.gay>
2025-07-23 12:28:44 -0400

    Adjust formatting of filter description

--- client/src/components/ScheduleTimeline/FilterDescription.vue
+++ client/src/components/ScheduleTimeline/FilterDescription.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { type FilterCriteria } from "@/composables/useFilterQuery";
 import CategoryLabel from "@/components/system/CategoryLabel.vue";
 
@@ -6,24 +7,24 @@ const props = defineProps<{
   criteria: FilterCriteria;
   allCategories: Array<string>;
 }>();
+
+const hasCategories = computed(() => props.criteria.categories.length > 0);
+const hasTags = computed(() => props.criteria.tags.length > 0);
 </script>
 
 <template>
   <div class="flex flex-col lg:flex-row flex-wrap gap-6">
-    <div class="flex flex-col gap-2 lg:basis-1/2">
-      <span v-if="props.criteria.categories.length > 0 || props.criteria.tags.length > 0">
-        Only showing events in:
-      </span>
-      <span class="ms-2 flex flex-wrap gap-3">
+    <div v-if="hasCategories" class="flex flex-col gap-2 lg:basis-1/2">
+      <span>Only showing events in:</span>
+      <span v-if="hasCategories" class="ms-2 flex flex-wrap gap-3">
         <span v-for="(category, index) in props.criteria.categories" :key="index">
           <CategoryLabel :title="category" :all-categories="props.allCategories" display="active" />
         </span>
       </span>
     </div>
-    <div class="flex flex-col gap-2">
-      <span v-if="props.criteria.categories.length > 0 && props.criteria.tags.length > 0">
-        And:
-      </span>
+    <div v-if="hasTags" class="flex flex-col gap-2">
+      <span v-if="hasCategories">And:</span>
+      <span v-else>Only showing events in:</span>
       <span class="ms-2 flex flex-wrap gap-3">
         <span v-for="(tag, index) in props.criteria.tags" :key="index">
           <CategoryLabel :title="tag" display="active" />
