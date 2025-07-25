6176450ddfffb6a827c13967c01c6c8ed45925ac
Lark Aster <lark@lark.gay>
2025-07-24 22:16:41 -0400

    Cache CDN requests in the service worker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -14,17 +14,61 @@ export default defineConfig({
     vueDevTools(),
     cloudflare(),
     tailwindcss(),
+    // This plugin installs a service worker to allow this app to work offline.
     VitePWA({
-      // We generate the manifest dynamically in the app.
+      // We generate the manifest dynamically in the app, so don't let the
+      // plugin do it.
       manifest: false,
       // Normally, this plugin automatically includes icons linked in the
       // manifest. However, because we're generating the manifest ourselves, we
       // need to tell it where to find them.
-      includeAssets: ["images/*"],
-      // Because the Bootstrap icon fonts do not live in the `public/`
-      // directory, we need to tell the plugin to include them.
+      includeAssets: ["icons/*"],
       workbox: {
-        globPatterns: ["**/*.{js,css,html,woff,woff2}"],
+        // Cache assets fetched from CDNs.
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "google-fonts-cache",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "gstatic-fonts-cache",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "jsdelivr-cache",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
       },
     }),
   ],
