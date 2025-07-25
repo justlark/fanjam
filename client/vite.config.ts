b852d77f2277f4a76883cc64d9887a7c1a09a88b
Lark Aster <lark@lark.gay>
2025-07-24 21:24:45 -0400

    Add service worker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,12 +4,19 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), cloudflare(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    cloudflare(),
+    tailwindcss(),
+    // We generate the manifest dynamically in the app.
+    VitePWA({ manifest: false }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
