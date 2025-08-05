adcc770d5c198b0f30dd8208d42f80ff3c7303d6
Lark Aster <lark@lark.gay>
2025-08-04 22:47:26 -0400

    Show current time slot in UI

--- client/src/components/ScheduleTimeline/ScheduleTimeSlot.vue
+++ client/src/components/ScheduleTimeline/ScheduleTimeSlot.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { useId } from "vue";
+import { useId, computed } from "vue";
 import Divider from "primevue/divider";
 import useFilterQuery, { toFilterQueryParams } from "@/composables/useFilterQuery";
 import { type Event } from "@/utils/api";
+import { dateIsBetween } from "@/utils/time";
 import CategoryLabel from "@/components/system/CategoryLabel.vue";
 import { RouterLink } from "vue-router";
 
@@ -15,12 +16,40 @@ const props = defineProps<{
 }>();
 
 const sectionHeadingId = useId();
+
+const isCurrentTimeSlot = computed(() => {
+  if (props.events.length === 0) return false;
+
+  const firstEventStartTime = props.events[0].startTime;
+  const lastEvent = props.events[props.events.length - 1];
+  const lastEventEndTime = lastEvent.endTime ?? lastEvent.startTime;
+  const now = new Date();
+
+  return dateIsBetween(now, firstEventStartTime, lastEventEndTime);
+});
 </script>
 
 <template>
   <section :aria-labelledby="sectionHeadingId">
-    <h2 :id="sectionHeadingId" class="text-xl">{{ props.localizedTime }}</h2>
-    <Divider pt:root="!mt-1" />
+    <div class="flex items-center gap-2">
+      <h2
+        :id="sectionHeadingId"
+        :class="{
+          'text-xl': true,
+          'font-bold': isCurrentTimeSlot,
+        }"
+      >
+        {{ props.localizedTime }}
+      </h2>
+      <small v-if="isCurrentTimeSlot" class="text-muted-color">now</small>
+    </div>
+    <Divider
+      :class="{
+        '!mt-1': true,
+        'before:!border-primary': isCurrentTimeSlot,
+        'before:!border-1': isCurrentTimeSlot,
+      }"
+    />
     <ul class="flex flex-wrap gap-3">
       <li v-for="event in props.events" :key="event.id">
         <RouterLink
