225c265fb30600ac2b6f37053f110ba9abc18c71
Lark Aster <lark@lark.gay>
2025-08-05 17:06:11 -0400

    Change dark mode primary color again

--- client/src/main.ts
+++ client/src/main.ts
@@ -32,17 +32,32 @@ const preset = definePreset(Aura, {
       },
       dark: {
         primary: {
-          50: "{orange.50}",
-          100: "{orange.100}",
-          200: "{orange.200}",
-          300: "{orange.300}",
-          400: "{orange.400}",
-          500: "{orange.500}",
-          600: "{orange.600}",
-          700: "{orange.700}",
-          800: "{orange.800}",
-          900: "{orange.900}",
-          950: "{orange.950}",
+          color: "{indigo.300}",
+          hoverColor: "{indigo.200}",
+          50: "{indigo.50}",
+          100: "{indigo.100}",
+          200: "{indigo.200}",
+          300: "{indigo.300}",
+          400: "{indigo.400}",
+          500: "{indigo.500}",
+          600: "{indigo.600}",
+          700: "{indigo.700}",
+          800: "{indigo.800}",
+          900: "{indigo.900}",
+          950: "{indigo.950}",
+        },
+      },
+    },
+  },
+  components: {
+    button: {
+      colorScheme: {
+        dark: {
+          outlined: {
+            primary: {
+              borderColor: "{primary.color}",
+            },
+          },
         },
       },
     },
