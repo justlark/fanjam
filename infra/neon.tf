f9d539205db1e36d623e9037277670f64d9e6cde
Lark Aster <lark@lark.gay>
2025-08-22 10:05:16 -0400

    Fix Terraform wanting to destroy the Neon database

--- infra/neon.tf
+++ infra/neon.tf
@@ -2,12 +2,17 @@ locals {
   neon_default_branch_name = "main"
 }
 
-// TODO: Once this issue in the Neon Terraform provider is addressed, we can
-// protect the default branch so it cannot be deleted.
-//
-// https://github.com/kislerdm/terraform-provider-neon/issues/153
+# TODO: Once this issue in the Neon Terraform provider is addressed, we can
+# protect the default branch so it cannot be deleted.
+#
+# https://github.com/kislerdm/terraform-provider-neon/issues/153
 
 resource "neon_project" "env" {
+  lifecycle {
+    # We definitely do not want to accidentally destroy the database.
+    prevent_destroy = true
+  }
+
   for_each  = local.environments
   name      = each.key
   org_id    = var.neon_org_id
@@ -27,6 +32,18 @@ resource "neon_project" "env" {
 }
 
 resource "neon_database" "sparklefish" {
+  lifecycle {
+    prevent_destroy = true
+
+    # When the cluster is restored from a snapshot via the Neon API (which is a
+    # normal thing we may want to do), that changes the default branch ID,
+    # which due to the way this provider works, would then destroy and replace
+    # the database on the next apply (which we definitely do not want). So, we
+    # need to tell Terraform to not consider the branch ID when deciding
+    # whether to apply changes.
+    ignore_changes = [branch_id]
+  }
+
   for_each   = local.environments
   project_id = neon_project.env[each.key].id
   branch_id  = neon_project.env[each.key].default_branch_id
@@ -35,6 +52,11 @@ resource "neon_database" "sparklefish" {
 }
 
 resource "neon_database" "sparklefish_shadow" {
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [branch_id]
+  }
+
   for_each   = local.environments
   project_id = neon_project.env[each.key].id
   branch_id  = neon_project.env[each.key].default_branch_id
