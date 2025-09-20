3d49cb768f9dbd2f45d3ee95362fdddde7f9d9f9
Lark Aster <lark@lark.gay>
2025-09-20 08:22:35 -0400

    Replace the KV cache with an in-memory cache

--- server/src/config.rs
+++ server/src/config.rs
@@ -17,7 +17,8 @@ struct Config {
     neon_default_branch_name: String,
     upstash_endpoint: String,
     upstash_api_token: upstash::ApiToken,
-    noco_default_buffer_cache_ttl_seconds: u32,
+    noco_default_memory_cache_ttl_millis: u32,
+    noco_kv_cache_ttl_seconds: u32,
     noco_summary_cache_ttl_seconds: u32,
 }
 
@@ -38,10 +39,11 @@ pub fn init(env: &Env) -> anyhow::Result<()> {
             upstash_api_token: upstash::ApiToken::from(
                 env.secret("UPSTASH_API_TOKEN")?.to_string(),
             ),
-            noco_default_buffer_cache_ttl_seconds: env
-                .var("NOCO_DEFAULT_BUFFER_CACHE_TTL_MILLIS")?
+            noco_default_memory_cache_ttl_millis: env
+                .var("NOCO_DEFAULT_MEMORY_CACHE_TTL_MILLIS")?
                 .to_string()
                 .parse()?,
+            noco_kv_cache_ttl_seconds: env.var("NOCO_KV_CACHE_TTL_SECONDS")?.to_string().parse()?,
             noco_summary_cache_ttl_seconds: env
                 .var("NOCO_SUMMARY_CACHE_TTL_SECONDS")?
                 .to_string()
@@ -88,8 +90,12 @@ pub fn upstash_api_token() -> upstash::ApiToken {
     get_config().upstash_api_token.clone()
 }
 
-pub fn noco_default_buffer_cache_ttl() -> Duration {
-    Duration::from_millis(get_config().noco_default_buffer_cache_ttl_seconds.into())
+pub fn noco_default_memory_cache_ttl() -> Duration {
+    Duration::from_millis(get_config().noco_default_memory_cache_ttl_millis.into())
+}
+
+pub fn noco_kv_cache_ttl() -> Duration {
+    Duration::from_secs(get_config().noco_kv_cache_ttl_seconds.into())
 }
 
 pub fn noco_summary_cache_ttl() -> Duration {
