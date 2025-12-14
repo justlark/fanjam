ff44f157d32c6b7204962fb26c6acdded7cee696
Lark Aster <lark@lark.gay>
2025-12-13 20:06:36 -0500

    Use semantic element for footer

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -4,70 +4,71 @@ import Button from "primevue/button";
 </script>
 
 <template>
-  <main class="flex flex-col justify-center items-center gap-8 p-6 min-h-screen">
-    <h1 class="text-8xl text-center mt-8 lg:mt-14 mb-8 font-black">
-      <span class="text-orange-400 dark:text-orange-300">Fan</span
-      ><span class="text-sky-400 dark:text-sky-300">Jam</span>
-    </h1>
-    <div class="text-2xl text-center">
-      <p>FanJam is an event planning app for conventions.</p>
-    </div>
-    <div class="text-lg text-justify max-w-4xl">
-      <p>
-        For small to medium-size cons, FanJam bridges the gap between a single person with a
-        spreadsheet and the kinds of people who can afford enterprise event planning software. Free
-        for small cons.
-      </p>
-    </div>
-    <Button
-      as="a"
-      label="Open Demo"
-      icon="pi pi-external-link"
-      icon-pos="right"
-      raised
-      target="_blank"
-      href="https://fanjam.live/app/804008/"
-    />
-    <Card>
-      <template #content>
-        <div class="text-xl text-center">
-          <p>Reach out if you're interested!</p>
-          <p>
-            <a
-              class="text-primary decoration-2 underline underline-offset-2 hover:decoration-3 font-bold"
-              href="mailto:lark@fanjam.live"
-              >lark@fanjam.live</a
-            >
-          </p>
-        </div>
-      </template>
-    </Card>
-    <div class="flex flex-wrap gap-8 justify-evenly">
-      <Card class="overflow-hidden">
-        <template #header>
-          <img
-            class="max-h-100 object-contain"
-            alt="Organizer app screenshot"
-            src="/images/nocodb-demo-screenshot.webp"
-          />
-        </template>
-        <template #title>For Organizers</template>
-        <template #subtitle>
-          A Google Docs-like spreadsheet app for putting together the con schedule
-        </template>
+  <div class="flex flex-col gap-6 p-6 min-h-screen">
+    <main class="flex flex-col justify-center items-center gap-8">
+      <h1 class="text-8xl text-center mt-8 lg:mt-14 mb-8 font-black">
+        <span class="text-orange-400 dark:text-orange-300">Fan</span
+        ><span class="text-sky-400 dark:text-sky-300">Jam</span>
+      </h1>
+      <div class="text-2xl text-center">
+        <p>FanJam is an event planning app for conventions.</p>
+      </div>
+      <div class="text-lg text-justify max-w-4xl">
+        <p>
+          For small to medium-size cons, FanJam bridges the gap between a single person with a
+          spreadsheet and the kinds of people who can afford enterprise event planning software.
+          Free for small cons.
+        </p>
+      </div>
+      <Button
+        as="a"
+        label="Open Demo"
+        icon="pi pi-external-link"
+        icon-pos="right"
+        raised
+        target="_blank"
+        href="https://fanjam.live/app/804008/"
+      />
+      <Card>
         <template #content>
-          <ul class="mb-2 list-disc list-inside">
-            <li>Keep track of who's running what, when, and where.</li>
-            <li>Publish event info, FAQs, policies, maps, socials, flyers, and anything else.</li>
-            <li>Have as many organizers in the app as you want, and control who has access.</li>
-            <li>Leave comments and notes that only organizers can see.</li>
-          </ul>
+          <div class="text-xl text-center">
+            <p>Reach out if you're interested!</p>
+            <p>
+              <a
+                class="text-primary decoration-2 underline underline-offset-2 hover:decoration-3 font-bold"
+                href="mailto:lark@fanjam.live"
+                >lark@fanjam.live</a
+              >
+            </p>
+          </div>
         </template>
-        <!--
+      </Card>
+      <div class="flex flex-wrap gap-8 justify-evenly">
+        <Card class="overflow-hidden">
+          <template #header>
+            <img
+              class="max-h-100 object-contain"
+              alt="Organizer app screenshot"
+              src="/images/nocodb-demo-screenshot.webp"
+            />
+          </template>
+          <template #title>For Organizers</template>
+          <template #subtitle>
+            A Google Docs-like spreadsheet app for putting together the con schedule
+          </template>
+          <template #content>
+            <ul class="mb-2 list-disc list-inside">
+              <li>Keep track of who's running what, when, and where.</li>
+              <li>Publish event info, FAQs, policies, maps, socials, flyers, and anything else.</li>
+              <li>Have as many organizers in the app as you want, and control who has access.</li>
+              <li>Leave comments and notes that only organizers can see.</li>
+            </ul>
+          </template>
+          <!--
         We're having stability issues with NocoDB right now, so we're removing
         the demo link for now.
         -->
-        <!--
+          <!--
         <template #footer>
           <div class="flex flex-wrap gap-6 items-center">
             <Button
@@ -86,38 +87,38 @@ import Button from "primevue/button";
           </div>
         </template>
         -->
-      </Card>
-      <Card class="overflow-hidden">
-        <template #header>
-          <div class="h-100 object-contain flex">
-            <img
-              class="w-full"
-              alt="Attendee app screenshot"
-              src="/images/fanjam-schedule-demo-screenshot.webp"
-            />
-            <img
-              class="w-full"
-              alt="Attendee app screenshot"
-              src="/images/fanjam-info-demo-screenshot.webp"
-            />
-          </div>
-        </template>
-        <template #title>For Attendees</template>
-        <template #subtitle>A website and mobile app for viewing the con schedule</template>
-        <template #content>
-          <ul class="mb-2 list-disc list-inside">
-            <li>See the schedule of events.</li>
-            <li>Filter, search, and build a personal itinerary.</li>
-            <li>See live announcements from the organizers.</li>
-            <li>Works completely offline.</li>
-          </ul>
-        </template>
-        <!--
+        </Card>
+        <Card class="overflow-hidden">
+          <template #header>
+            <div class="h-100 object-contain flex">
+              <img
+                class="w-full"
+                alt="Attendee app screenshot"
+                src="/images/fanjam-schedule-demo-screenshot.webp"
+              />
+              <img
+                class="w-full"
+                alt="Attendee app screenshot"
+                src="/images/fanjam-info-demo-screenshot.webp"
+              />
+            </div>
+          </template>
+          <template #title>For Attendees</template>
+          <template #subtitle>A website and mobile app for viewing the con schedule</template>
+          <template #content>
+            <ul class="mb-2 list-disc list-inside">
+              <li>See the schedule of events.</li>
+              <li>Filter, search, and build a personal itinerary.</li>
+              <li>See live announcements from the organizers.</li>
+              <li>Works completely offline.</li>
+            </ul>
+          </template>
+          <!--
           Because we're only showing the attendee demo on the homepage right
           now, we're moving it up to the top to make it more obvious. Once both
           demos are online, we can put this back here.
         -->
-        <!--
+          <!--
         <template #footer>
           <div class="flex flex-wrap gap-6 items-center">
             <Button
@@ -136,9 +137,10 @@ import Button from "primevue/button";
           </div>
         </template>
         -->
-      </Card>
-    </div>
-    <div class="mt-auto flex flex-col items-center gap-1 text-muted-color">
+        </Card>
+      </div>
+    </main>
+    <footer class="mt-auto flex flex-col items-center gap-1 text-muted-color">
       <small>
         <a
           class="text-primary decoration-2 underline underline-offset-2 hover:decoration-3"
@@ -149,6 +151,6 @@ import Button from "primevue/button";
         </a>
       </small>
       <small>© 2025 Lark Aster</small>
-    </div>
-  </main>
+    </footer>
+  </div>
 </template>
