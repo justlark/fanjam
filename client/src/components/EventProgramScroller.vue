34f4451cfad042a09f038b1932643057b92c41d8
Lark Aster <lark@lark.gay>
2025-12-22 18:38:38 -0500

    Remove unused component prop

--- client/src/components/EventProgramScroller.vue
+++ client/src/components/EventProgramScroller.vue
@@ -15,7 +15,6 @@ const props = defineProps<{
   <VirtualScroller :items="[...props.filteredEvents]" :item-size="50" scroll-height="100%">
     <template #item="{ item, options }">
       <EventProgramDescription
-        :key="item.id"
         :event="item"
         :expand="item.id === props.focusedEventId"
         :all-categories="props.allCategories"
