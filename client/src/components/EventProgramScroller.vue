76ed27b36c4e4ceb035fa062994e075b0f017fe3
Lark Aster <lark@lark.gay>
2025-12-23 09:10:28 -0500

    Fix virtual scroller item size

--- client/src/components/EventProgramScroller.vue
+++ client/src/components/EventProgramScroller.vue
@@ -16,8 +16,8 @@ const rowVirtualizer = useVirtualizer({
   count: props.filteredEvents.length,
   getScrollElement: () => scrollerRef.value,
   // Keep this in sync with `EventProgramDescription`. Use the browser dev
-  // tools to measure if needed.
-  estimateSize: () => 94,
+  // tools to measure if needed. Make sure you include the margin.
+  estimateSize: () => 114,
   getItemKey: (index) => props.filteredEvents[index].id,
   overscan: 5,
 });
