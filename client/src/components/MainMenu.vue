e60f106d469b20c4a30a3ccee59814fa3e34193f
Lark Aster <lark@lark.gay>
2025-12-25 06:54:14 -0500

    Fix main menu nav

--- client/src/components/MainMenu.vue
+++ client/src/components/MainMenu.vue
@@ -1,16 +1,8 @@
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
 import MainMenuItem from "./MainMenuItem.vue";
 import useUnreadAnnouncements from "@/composables/useUnreadAnnouncements";
 
-const fromRoute = ref<string>("schedule");
 const unreadAnnouncements = useUnreadAnnouncements();
-
-onMounted(() => {
-  if (history.state.from !== undefined) {
-    fromRoute.value = history.state.from;
-  }
-});
 </script>
 
 <template>
@@ -19,17 +11,7 @@ onMounted(() => {
       icon="bi bi-calendar-event"
       label="Schedule"
       to="schedule"
-      :is-active="
-        (route) => route.name === 'schedule' || (route.name === 'event' && fromRoute === 'schedule')
-      "
-    />
-    <MainMenuItem
-      icon="bi bi-book"
-      label="All Events"
-      to="program"
-      :is-active="
-        (route) => route.name === 'program' || (route.name === 'event' && fromRoute === 'program')
-      "
+      :is-active="(route) => route.name === 'schedule' || route.name === 'event'"
     />
     <MainMenuItem
       icon="bi bi-megaphone"
