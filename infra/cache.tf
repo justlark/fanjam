f7ac24f9d48f09c9c0fb24d74daf294029841b9a
Lark Aster <lark@lark.gay>
2025-12-30 10:05:02 -0500

    Refactor Tofu

--- infra/cache.tf
+++ infra/cache.tf
@@ -1,3 +1,8 @@
+locals {
+  api_subdomains = [for stage in local.stages : stage == "prod" ? "api" : "api-${stage}"]
+  api_hosts      = [for subdomain in local.api_subdomains : "${subdomain}.${data.cloudflare_zone.site.name}"]
+}
+
 resource "cloudflare_ruleset" "etags" {
   zone_id = data.cloudflare_zone.site.zone_id
   name    = "strong_etags"
@@ -11,7 +16,7 @@ resource "cloudflare_ruleset" "etags" {
       respect_strong_etags = true
     }
 
-    expression  = "(http.host in {\"api.${data.cloudflare_zone.site.name}\" \"api-test.${data.cloudflare_zone.site.name}\"})"
+    expression  = "(http.host in {${join(" ", [for api_host in local.api_hosts : "\"${api_host}\""])}})"
     description = "Enable Strong ETags"
     enabled     = true
   }
