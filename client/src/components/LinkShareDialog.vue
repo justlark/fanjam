a168d7df8015b59734949410366fa4e1497344e8
Lark Aster <lark@lark.gay>
2026-02-26 14:20:14 -0500

    Refactor out link share dialog

---
+++ client/src/components/LinkShareDialog.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import InputText from "primevue/inputtext";
+import InputGroup from "primevue/inputgroup";
+import InputGroupAddon from "primevue/inputgroupaddon";
+import Button from "primevue/button";
+import QrcodeVue from "qrcode.vue";
+import Dialog from "primevue/dialog";
+import { useToast } from "primevue/usetoast";
+
+const visible = defineModel<boolean>("visible", {
+  type: Boolean,
+  required: true,
+});
+
+const toast = useToast();
+
+const props = defineProps<{
+  link: string;
+  toastMessage: string;
+}>();
+
+const copyShareUrl = async () => {
+  await navigator.clipboard.writeText(props.link);
+
+  toast.add({
+    severity: "info",
+    summary: "Link Copied",
+    detail: props.toastMessage,
+    life: 1500,
+  });
+};
+</script>
+
+<template>
+  <Dialog
+    class="max-w-100 m-4"
+    v-model:visible="visible"
+    modal
+    dismissable-mask
+    header="Share Schedule"
+  >
+    <p><slot /></p>
+    <div class="rounded-3xl overflow-hidden mx-auto w-fit my-6 shadow-md">
+      <QrcodeVue
+        :value="props.link"
+        :margin="3"
+        :size="220"
+        foreground="#18181b"
+        background="#f1f5f9"
+        render-as="svg"
+      />
+    </div>
+    <InputGroup>
+      <InputGroupAddon>
+        <Button @click="copyShareUrl" label="Copy" icon="bi bi-clipboard" />
+      </InputGroupAddon>
+      <InputText :value="props.link" disabled />
+    </InputGroup>
+  </Dialog>
+</template>
--- client/src/components/ScheduleShareModal.vue
+++ client/src/components/ScheduleShareModal.vue
@@ -1,14 +1,8 @@
 <script setup lang="ts">
 import { computed } from "vue";
-import Dialog from "primevue/dialog";
-import InputText from "primevue/inputtext";
-import InputGroup from "primevue/inputgroup";
-import InputGroupAddon from "primevue/inputgroupaddon";
-import Button from "primevue/button";
-import QrcodeVue from "qrcode.vue";
 import useStarredEvents from "@/composables/useStarredEvents";
-import { useToast } from "primevue/usetoast";
 import { encodeBase64url } from "@/utils/encoding";
+import LinkShareDialog from "@/components/LinkShareDialog.vue";
 
 const visible = defineModel<boolean>("visible", {
   type: Boolean,
@@ -16,52 +10,20 @@ const visible = defineModel<boolean>("visible", {
 });
 
 const starredEvents = useStarredEvents();
-const toast = useToast();
 
 const shareUrl = computed(() => {
   const starredEventIds = [...starredEvents.value];
   starredEventIds.sort();
   return `${window.location.origin}/share/?s=${encodeBase64url(starredEventIds.join(","))}`;
 });
-
-const copyShareUrl = async () => {
-  await navigator.clipboard.writeText(shareUrl.value);
-
-  toast.add({
-    severity: "info",
-    summary: "Link Copied",
-    detail: "Share this URL to share your schedule.",
-    life: 1500,
-  });
-};
 </script>
 
 <template>
-  <Dialog
-    class="max-w-100 m-4"
+  <LinkShareDialog
     v-model:visible="visible"
-    modal
-    dismissable-mask
-    header="Share Schedule"
+    :link="shareUrl"
+    toast-message="Share this URL to share your schedule."
   >
-    <p>Share your schedule with a friend or move it to another device.</p>
-    <div class="rounded-3xl overflow-hidden mx-auto w-fit my-6 shadow-md">
-      <QrcodeVue
-        :value="shareUrl"
-        :margin="3"
-        :size="220"
-        foreground="#18181b"
-        background="#f1f5f9"
-        render-as="svg"
-      />
-    </div>
-    <template #footer>
-      <InputGroup>
-        <InputGroupAddon>
-          <Button @click="copyShareUrl" label="Copy" icon="bi bi-clipboard" />
-        </InputGroupAddon>
-        <InputText :value="shareUrl" disabled />
-      </InputGroup>
-    </template>
-  </Dialog>
+    Share your schedule with a friend or move it to another device.
+  </LinkShareDialog>
 </template>
