38e8acb8cfd12501e1c269626b513851bd1978f1
Lark Aster <lark@lark.gay>
2026-05-30 22:04:54 -0400

    Loosen CORS requirements

--- server/src/cors.rs
+++ server/src/cors.rs
@@ -1,34 +1,19 @@
 use axum::http::{
-    HeaderName, Method,
-    header::{AUTHORIZATION, CONTENT_TYPE, ETAG, IF_NONE_MATCH},
+    Method,
+    header::{CONTENT_TYPE, ETAG, IF_NONE_MATCH},
 };
-use tower_http::cors::CorsLayer;
-
-use crate::config;
-
-const LOCAL_HOST: &str = "http://localhost:5173";
-
-const CORS_ALLOWED_METHODS: [Method; 5] = [
-    Method::GET,
-    Method::POST,
-    Method::PUT,
-    Method::PATCH,
-    Method::DELETE,
-];
-
-const CORS_ALLOWED_HEADERS: [HeaderName; 3] = [CONTENT_TYPE, AUTHORIZATION, IF_NONE_MATCH];
-
-const CORS_EXPOSED_HEADERS: [HeaderName; 1] = [ETAG];
+use tower_http::cors::{Any, CorsLayer};
 
 pub fn cors_layer() -> CorsLayer {
     CorsLayer::new()
-        .allow_methods(CORS_ALLOWED_METHODS)
-        .allow_headers(CORS_ALLOWED_HEADERS)
-        .expose_headers(CORS_EXPOSED_HEADERS)
-        .allow_origin([
-            format!("https://{}", config::client_domain())
-                .parse()
-                .unwrap(),
-            LOCAL_HOST.parse().unwrap(),
+        .allow_methods([
+            Method::GET,
+            Method::POST,
+            Method::PUT,
+            Method::PATCH,
+            Method::DELETE,
         ])
+        .allow_headers([CONTENT_TYPE, IF_NONE_MATCH])
+        .expose_headers([ETAG])
+        .allow_origin(Any)
 }
