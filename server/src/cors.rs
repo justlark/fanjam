0c039a02d3b88cb4444e707560eee0e6daf4c4db
Lark Aster <lark@lark.gay>
2026-08-25 04:38:58 -0400

    Add CORS max age to avoid unnecessary preflight requests

--- server/src/cors.rs
+++ server/src/cors.rs
@@ -1,8 +1,14 @@
+use std::time::Duration;
+
 use axum::http::{
     Method,
     header::{CONTENT_TYPE, ETAG, IF_NONE_MATCH},
 };
 use tower_http::cors::{Any, CorsLayer};
+
+// It is unlikely that the CORS headers will change frequently, so we can avoid unnecessary
+// preflight requests.
+const PREFLIGHT_MAX_AGE: Duration = Duration::from_secs(60 * 60);
 
 pub fn cors_layer() -> CorsLayer {
     CorsLayer::new()
@@ -15,5 +21,6 @@ pub fn cors_layer() -> CorsLayer {
         ])
         .allow_headers([CONTENT_TYPE, IF_NONE_MATCH])
         .expose_headers([ETAG])
+        .max_age(PREFLIGHT_MAX_AGE)
         .allow_origin(Any)
 }
